3d7a66c4834aec9ce827098353f6a92d14f9d316
SatyamJadhav007 <satyam.d.jadhav@gmail.com>
2025-08-10 11:35:15 +0530

    Add Convex Package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,19 @@
+"use client";
 import { Button } from "@workspace/ui/components/button";
 import { add } from "@workspace/math/add";
 import { Input } from "@workspace/ui/components/input";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api"; //its funny how i missed the "/api" import here...
+//How did i imported packages from the backend to the front-end here??
+//first=>Update the package.json dependencies, second=>Update the tsconfig.json paths with the desired URL,three=>Update the export for the package.json of the exporting workspace and also set the paths of the tsconfig.json file's paths
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
-        <Input />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/web</p>
+      <Button onClick={() => addUser()}>Add</Button>
+      <div className="max-w-sm w-full mx-auto">{JSON.stringify(users)}</div>
     </div>
   );
 }
