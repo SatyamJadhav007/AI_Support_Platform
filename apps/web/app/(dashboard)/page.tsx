1f44871e2e9531a3b4f191260d32ef8f334de2af
SatyamJadhav007 <satyam.d.jadhav@gmail.com>
2025-08-11 09:40:14 +0530

    Add Orgaizations

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Button } from "@workspace/ui/components/button";
 import { add } from "@workspace/math/add";
 import { Input } from "@workspace/ui/components/input";
-import {
-  useMutation,
-  useQuery,
-  Authenticated,
-  Unauthenticated,
-} from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api"; //its funny how i missed the "/api" import here...
 //How did i imported packages from the backend to the front-end here??
 //first=>Update the package.json dependencies, second=>Update the tsconfig.json paths with the desired URL,three=>Update the export for the package.json of the exporting workspace and also set the paths of the tsconfig.json file's paths
@@ -17,18 +12,13 @@ export default function Page() {
   const addUser = useMutation(api.users.add);
   return (
     <>
-      <Authenticated>
-        <div className="flex flex-col items-center justify-center min-h-svh">
-          <p>apps/web</p>
-          <UserButton />
-          <Button onClick={() => addUser()}>Add</Button>
-          <div className="max-w-sm w-full mx-auto">{JSON.stringify(users)}</div>
-        </div>
-      </Authenticated>
-      <Unauthenticated>
-        <p>Must be signed in!</p>
-        <SignInButton>Sign in!</SignInButton>
-      </Unauthenticated>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p>apps/web</p>
+        <UserButton />
+        <OrganizationSwitcher hidePersonal />
+        <Button onClick={() => addUser()}>Add</Button>
+        <div className="max-w-sm w-full mx-auto">{JSON.stringify(users)}</div>
+      </div>
     </>
   );
 }
